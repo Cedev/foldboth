9c96d0cc79dd5eb5a390bac0fb7bd3771beb7e86
Cedric Shock <cedricshock@gmail.com>
2018-05-04 04:33:06 +0000

    WIP: version of foldboth that makes decisions based only on what came from the left

--- src/Data/List/FoldBoth.hs
+++ src/Data/List/FoldBoth.hs
@@ -14,33 +14,33 @@ import GHC.Exts (oneShot)
 
 -- | Fold from both the left and right sides at the same time.
 {-# INLINE foldBoth #-}
-foldBoth :: Foldable t => (l -> a -> r -> (l, r)) -> l -> r -> t a -> (l, r)
+foldBoth :: Foldable t => (l -> a -> (l, r -> r)) -> l -> r -> t a -> (l, r)
 foldBoth k lz rz xs = foldBothBy (folded xs) k lz rz
 
 -- | A version of 'foldBoth' that is strict in the left argument
 {-# INLINE foldBoth' #-}
-foldBoth' :: Foldable t => (l -> a -> r -> (l , r)) -> l -> r -> t a -> (l, r)
+foldBoth' :: Foldable t =>  (l -> a -> (l, r -> r)) -> l -> r -> t a -> (l, r)
 foldBoth' k lz rz xs = foldBothBy' (folded xs) k lz rz
     
 type FoldR a b = (a -> b -> b) -> b -> b
 
 -- | A version of 'foldBoth' that doesn't require a Foldable
 {-# INLINE foldBothBy #-}
-foldBothBy :: FoldR a (l -> (l, r)) -> (l -> a -> r -> (l, r)) -> l -> r -> (l, r)
+foldBothBy :: FoldR a (l -> (l, r)) ->  (l -> a -> (l, r -> r)) -> l -> r -> (l, r)
 foldBothBy foldr k lz rz = 
   foldr (\x fn -> oneShot (\l -> 
-                                let ~(l1, r1) = fn l2
-                                    ~(l2, r2) = k l x r1
-                                    in (l1, r2))) (\l -> (l, rz)) lz
+                                let (l1, r) = fn l2
+                                    (l2, fr) = k l x
+                                    in (l1, fr r))) (\l -> (l, rz)) lz
                                     
 -- | Strict version of 'foldBothBy''
 {-# INLINE foldBothBy' #-}
-foldBothBy' :: FoldR a (l -> (l, r)) -> (l -> a -> r -> (l, r)) -> l -> r -> (l, r)
+foldBothBy' :: FoldR a (l -> (l, r)) -> (l -> a -> (l, r -> r)) -> l -> r -> (l, r)
 foldBothBy' foldr k lz rz = 
   foldr (\x fn -> oneShot (\l -> l `seq`
-                                let ~(l1, r1) = fn l2
-                                    ~(l2, r2) = k l x r1
-                                    in (l1, r2))) (\l -> (l, rz)) lz
+                                let (l1, r) = fn l2
+                                    (l2, fr) = k l x
+                                    in (l1, fr r))) (\l -> (l, rz)) lz
                                     
 -- | Rearrange the arguments to 'foldr' so that the structure it acts on is the first argument
 {-# INLINE folded #-}
